df40982f06f38f1857d1fc291817c023e19648c9
MatsuoTakuro <matsuo_takuro@retail-ai.jp>
2022-12-17 12:09:26 +0900

    get started with connect-go

---
+++ cmd/server/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"context"
+	greetv1 "example/gen/greet/v1"
+	"example/gen/greet/v1/greetv1connect"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/bufbuild/connect-go"
+	"golang.org/x/net/http2"
+	"golang.org/x/net/http2/h2c"
+)
+
+type GreetServer struct{}
+
+func (s *GreetServer) Greet(
+	ctx context.Context,
+	req *connect.Request[greetv1.GreetRequest],
+) (*connect.Response[greetv1.GreetResponse], error) {
+
+	log.Println("Request headers: ", req.Header())
+	res := connect.NewResponse(
+		&greetv1.GreetResponse{
+			Greeting: fmt.Sprintf("Hello, %s!", req.Msg.Name),
+		})
+	res.Header().Set("Greet-Version", "v1")
+	return res, nil
+}
+
+func main() {
+	greeter := &GreetServer{}
+	mux := http.NewServeMux()
+	// create path and handler from implementation of grpc service
+	path, handler := greetv1connect.NewGreetServiceHandler(greeter)
+	// register handler with path to mux
+	mux.Handle(path, handler)
+	http.ListenAndServe(
+		"localhost:8080",
+		// Use h2c so we can serve HTTP/2 without TLS.
+		h2c.NewHandler(mux, &http2.Server{}),
+	)
+}
---
+++ go.mod
@@ -0,0 +1,12 @@
+module example
+
+go 1.19
+
+require github.com/bufbuild/connect-go v1.4.0
+
+require golang.org/x/text v0.5.0 // indirect
+
+require (
+	golang.org/x/net v0.4.0
+	google.golang.org/protobuf v1.28.1 // indirect
+)
